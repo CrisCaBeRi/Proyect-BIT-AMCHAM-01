54788f4634c44f32150d790f6d4fe94346ec76d5
Cristian Betancourt <cristian.camilo.betancourt.rin@gmail.com>
2023-05-16 17:01:35 -0500

    All complete

---
+++ JS/actionButtons.js
@@ -0,0 +1,10 @@
+const EMAIL_FORM = document.getElementById("email-form");
+EMAIL_FORM.addEventListener("submit", function (event) {
+  alert("Gracias por registrar tu correo");
+});
+function navHistory() {
+  location.href = "../HTML/history.html";
+}
+function navToMembers() {
+  location.href = "../HTML/members.html";
+}
